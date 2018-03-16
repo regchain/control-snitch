47f1a180a57db8e57da08b37837462af7cc10170
Willy Nugraha <willymiiee@gmail.com>
2018-03-17 01:15:38 +0700

    rename HomeController to PageController

---
+++ src/Http/Controllers/PageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace EKejaksaan\Lapdu\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Config;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('lapdu::home');
+    }
+
+    public function lapdu()
+    {
+        return view('lapdu::guest.pages.cara_lapdu');
+    }
+
+    public function whistle()
+    {
+        return view('lapdu::guest.pages.cara_whistle');
+    }
+
+    public function question($page)
+    {
+        switch ($page) {
+            case 1:
+                return view('lapdu::guest.pages.question1');
+                break;
+
+            case 2:
+                return view('lapdu::guest.pages.question2');
+                break;
+
+            case 3:
+                return view('lapdu::guest.pages.question3');
+                break;
+
+            case 4:
+                return view('lapdu::guest.pages.question4');
+                break;
+
+            default:
+                # code...
+                break;
+        }
+        # code...
+    }
+}
