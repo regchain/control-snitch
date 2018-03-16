ec32dad68952fe79e31420778f8a57664e7405d5
Willy Nugraha <willymiiee@gmail.com>
2018-03-17 01:16:10 +0700

    add template views

---
+++ resources/views/templates/alpha/headeralt.blade.php
@@ -0,0 +1,31 @@
+<header id="header">
+    <h1><a href="{{ route('lapdu.home') }}">eLAPDU</a> | J.A.M. PENGAWASAN</h1>
+
+    <nav id="nav">
+        <ul>
+            <li><a href="{{ url('lapdu.home') }}">Awal</a></li>
+            <li>
+                <a href="#" class="icon fa-angle-down">Aplikasi</a>
+                <ul>
+                    <li><a href="{{ route('lapdu.howtolapdu') }}">Cara Pengaduan</a></li>
+                    <li><a href="{{ route('lapdu.howtowhistle') }}">Wishtle Blower</a></li>
+                    <li><a href="{{ route('lapdu.howtowhistle') }}">Perlindungan Saksi</a></li>
+                    <li><a href="{{ route('lapdu.question', ['page' => 1]) }}">Lapor..!</a></li>
+                    <li><a href="kawal">Periksa Pengaduan</a></li>
+                    {{-- <li>
+                        <a href="#">Masuk</a>
+                        <ul>
+                            <li><a href="#">Option One</a></li>
+                            <li><a href="#">Option Two</a></li>
+                            <li><a href="#">Option Three</a></li>
+                            <li><a href="#">Option Four</a></li>
+                        </ul>
+                    </li> --}}
+                </ul>
+            </li>
+            <li><a href="kawal" class="button">Masuk</a></li>
+            <li><a href="register" class="button">Buat Akun</a></li>
+        </ul>
+    </nav>
+</header>
+
---
+++ resources/views/templates/alpha/template.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<!--
+	This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html>
+<head>
+    <title>@yield('title') | e-LAPDU</title>
+    <meta charset="UTF-8">
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <meta name="csrf-token" content=" {{ csrf_token() }}">
+    @include('vendor.lapdu.templates.alpha.head')
+    @yield('stylesheets')
+</head>
+
+<body class="landing">
+    <div id="page-wrapper">
+        <!-- Header -->
+        @include('vendor.lapdu.templates.alpha.header')
+
+        <!-- Banner -->
+        @include('vendor.lapdu.guest.pages.banner')
+
+        <!-- Main -->
+        <section id="main" class="container">
+            <!-- Your Page Content Here -->
+            @yield('content')
+        </section>
+
+        <!-- Footer -->
+        @include('vendor.lapdu.templates.alpha.footer')
+    </div>
+
+    <!-- REQUIRED JS SCRIPTS -->
+    @include('vendor.lapdu.templates.alpha.scripts')
+
+    <!-- Optionally, you can add Slimscroll and FastClick plugins.
+        Both of these plugins are recommended to enhance the
+        user experience. -->
+    @yield('scripts')
+</body>
+</html>
