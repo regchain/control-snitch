bd03781c8e4b7c4253f545686bfc182fbfbf2833
Willy Nugraha <willymiiee@gmail.com>
2018-03-18 20:26:00 +0700

    update page controller

--- src/Http/Controllers/PageController.php
+++ src/Http/Controllers/PageController.php
@@ -2,8 +2,9 @@
 
 namespace EKejaksaan\Lapdu\Http\Controllers;
 
+use EKejaksaan\Lapdu\Models\Report;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Config;
+use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
@@ -22,23 +23,49 @@ class PageController extends Controller
         return view('lapdu::guest.pages.cara_whistle');
     }
 
-    public function question($page)
+    public function question($page, Request $request)
     {
         switch ($page) {
             case 1:
-                return view('lapdu::guest.pages.question1');
+                $data = new Report();
+                $data->save();
+                return view('lapdu::guest.pages.question1', compact('data'));
                 break;
 
             case 2:
-                return view('lapdu::guest.pages.question2');
+                if ($request->has('id')) {
+                    $data = Report::find($request->get('id'));
+
+                    if ($data) {
+                        return view('lapdu::guest.pages.question2', compact('data'));
+                    }
+                }
+
+                return redirect()->route('lapdu.question', ['page' => 1]);
                 break;
 
             case 3:
-                return view('lapdu::guest.pages.question3');
+                if ($request->has('id')) {
+                    $data = Report::find($request->get('id'));
+
+                    if ($data) {
+                        return view('lapdu::guest.pages.question3', compact('data'));
+                    }
+                }
+
+                return redirect()->route('lapdu.question', ['page' => 1]);
                 break;
 
             case 4:
-                return view('lapdu::guest.pages.question4');
+                if ($request->has('id')) {
+                    $data = Report::find($request->get('id'));
+
+                    if ($data) {
+                        return view('lapdu::guest.pages.question4', compact('data'));
+                    }
+                }
+
+                return redirect()->route('lapdu.question', ['page' => 1]);
                 break;
 
             default:
