83487cae9b59acf79fec2934c501760b344613fe
Willy Nugraha <willymiiee@gmail.com>
2018-03-18 19:41:42 +0700

    create report controller

---
+++ src/Http/Controllers/ReportController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace EKejaksaan\Lapdu\Http\Controllers;
+
+use EKejaksaan\Lapdu\Models\Report;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = Report::find($id);
+        $input = $request->except('_method');
+
+        foreach ($input as $k => $i) {
+            $data->$k = $i;
+        }
+
+        $data->save();
+        return redirect()->route('lapdu.question', ['page' => 4, 'id' => $id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
