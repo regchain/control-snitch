a69bb3b159261bdeafd9442dacea7a851043d95c
Willy Nugraha <willymiiee@gmail.com>
2018-03-19 11:33:33 +0700

    operator views

---
+++ resources/views/operator/home.blade.php
@@ -0,0 +1,42 @@
+@extends('vendor.lapdu.operator.template')
+
+@section('title', 'Dashboard')
+
+@section('judulhalaman', 'Kejaksaan Agung')
+
+@section('subjudul', 'Republik Indonesia')
+
+@section('stylesheets')
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="{{asset('vendor/core/bootstrap/dist/css/bootstrap.min.css')}}">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('vendor/core/font-awesome/css/font-awesome.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{asset('vendor/core/Ionicons/css/ionicons.min.css')}}">
+@endsection
+
+@section('content')
+    <!-- Default box -->
+    <div class="box">
+        <div class="box-header with-border">
+            <h2 class="box-title">EXECUTIVE INFORMATION SYSTEM</h2>
+        </div>
+
+        <div class="box-body">
+            <div class="row">
+                @include('vendor.lapdu.partials.dashboard.putusan')
+
+                @include('vendor.lapdu.partials.dashboard.subyek_hukum')
+
+                @include('vendor.lapdu.partials.dashboard.kasus')
+            </div>
+        </div>
+        <!-- /.box-body -->
+
+        <div class="box-footer">
+            &nbsp;
+        </div>
+        <!-- /.box-footer-->
+    </div>
+    <!-- /.box -->
+@endsection
---
+++ resources/views/operator/scripts.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery 3 -->
+<script src="{{ asset('vendor/core/jquery/dist/jquery.min.js')}}"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{ asset('vendor/core/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('vendor/core/admin-lte/dist/js/adminlte.min.js')}}"></script>
