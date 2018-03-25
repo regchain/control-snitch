e2311bdbed3e23af9e754901a3ddc543016015e9
Willy Nugraha <willymiiee@gmail.com>
2018-03-25 08:15:54 +0700

    add logic for add examiner

--- resources/views/klarifikasi/partials/_jaksapengawas_create.blade.php
+++ resources/views/klarifikasi/partials/_jaksapengawas_create.blade.php
@@ -1,145 +1,65 @@
-
 <div class="panel panel-default">
-  <!-- Default panel contents -->
-  <div class="panel-heading">
-    <div class="input-group">
-      {{-- <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span> --}}
-      <span id="inputGroupSuccess1Status" class="sr-only">(success)</span>
-      <select class="form-control select2" style="width: 100%;" id="ja">
-        <option selected="selected">Pilih Pemeriksa</option>
-        <option>[N.R.P] | [Nama Lengkap]</option>
-        <option>[N.R.P] | [Nama Lengkap]</option>
-        <option>[N.R.P] | [Nama Lengkap]</option>
-        <option>[N.R.P] | [Nama Lengkap]</option>
-        <option>[N.R.P] | [Nama Lengkap]</option>
-      </select>
-      <span class="input-group-addon btn btn-default"><a href="#"><i class="fa fa-plus"></i> Tambah</a></span>
-    </div>
-  </div>                 <!-- List group -->
-  <ul class="list-group">
-    <li class="list-group-item">
-      <div class="box-body table-responsive no-padding">
-        <table id="example2" class="table table-bordered table-striped">
-          <tbody>
-            <tr>
-              <td width="5%">1. </td>
-              <td width="15%"><strong>Nama</strong></td>
-              <td>: [Nama Lengkap]</td>
-              <td width="10%"><a href="#" class="btn btn-flat btn-xs btn-default"><i class="fa fa-remove"></i></a></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td><strong>Pangkat</strong></td>
-              <td>: [Pangkat], [Golongan] </td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td><strong>NIP / NRP</strong></td>
-              <td>: [123456] | [12344556788] </td>
-              <td></td>
-              <tr>
-                <td></td>
-                <td><strong>Jabatan</strong></td>
-                <td>: [Jabatan], [Satuan Kerja] </td>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
+    <!-- Default panel contents -->
+    <div class="panel-heading">
+        <div class="input-group">
+            {{-- <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span> --}}
+            <span id="inputGroupSuccess1Status" class="sr-only">(success)</span>
+
+            <select class="form-control select2" style="width: 100%;" id="ja">
+                <option selected="selected">Pilih Pemeriksa</option>
+                @foreach ($users as $u)
+                <option value="{{ $u->_id }}">{{ $u->nrp }} | {{ $u->name }}</option>
+                @endforeach
+            </select>
+
+            <span class="input-group-addon btn btn-default">
+                <a href="javascript:void(0)" id="add-pemeriksa"><i class="fa fa-plus"></i> Tambah</a>
+            </span>
         </div>
-      </li>
-      <li class="list-group-item">
-        <div class="box-body table-responsive no-padding">
-          <table id="example2" class="table table-bordered table-striped">
-            <tbody>
-              <tr>
-                <td width="5%">2. </td>
-                <td width="15%"><strong>Nama</strong></td>
-                <td>: [Nama Lengkap]</td>
-                <td width="10%"><a href="#" class="btn btn-flat btn-xs btn-default"><i class="fa fa-remove"></i></a></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td><strong>Pangkat</strong></td>
-                <td>: [Pangkat], [Golongan] </td>
-                <td></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td><strong>NIP / NRP</strong></td>
-                <td>: [123456] | [12344556788] </td>
-                <td></td>
-                <tr>
-                  <td></td>
-                  <td><strong>Jabatan</strong></td>
-                  <td>: [Jabatan], [Satuan Kerja] </td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </li>
-        <li class="list-group-item">
-          <div class="box-body table-responsive no-padding">
-            <table id="example2" class="table table-bordered table-striped">
-              <tbody>
-                <tr>
-                  <td width="5%">3. </td>
-                  <td width="15%"><strong>Nama</strong></td>
-                  <td>: [Nama Lengkap]</td>
-                  <td width="10%"><a href="#" class="btn btn-flat btn-xs btn-default"><i class="fa fa-remove"></i></a></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td><strong>Pangkat</strong></td>
-                  <td>: [Pangkat], [Golongan] </td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td><strong>NIP / NRP</strong></td>
-                  <td>: [123456] | [12344556788] </td>
-                  <td></td>
-                  <tr>
-                    <td></td>
-                    <td><strong>Jabatan</strong></td>
-                    <td>: [Jabatan], [Satuan Kerja] </td>
-                    <td></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </li>
-          <li class="list-group-item"><div class="box-body table-responsive no-padding">
-            <table id="example2" class="table table-bordered table-striped">
-              <tbody>
-                <tr>
-                  <td width="5%">4. </td>
-                  <td width="15%"><strong>Nama</strong></td>
-                  <td>: [Nama Lengkap]</td>
-                  <td width="10%"><a href="#" class="btn btn-flat btn-xs btn-default"><i class="fa fa-remove"></i></a></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td><strong>Pangkat</strong></td>
-                  <td>: [Pangkat], [Golongan] </td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td><strong>NIP / NRP</strong></td>
-                  <td>: [123456] | [12344556788] </td>
-                  <td></td>
-                  <tr>
-                    <td></td>
-                    <td><strong>Jabatan</strong></td>
-                    <td>: [Jabatan], [Satuan Kerja] </td>
-                    <td></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </li>
-        </ul>
-      </div>
-     
+    </div>
+    <!-- List group -->
+
+    <ul class="list-group" id="list-pemeriksa">
+        @if ($data->examiners)
+            @foreach ($data->examiners as $k => $e)
+            <li class="list-group-item">
+                <div class="box-body table-responsive no-padding">
+                    <table id="example2" class="table table-bordered table-striped">
+                        <tbody>
+                            <tr>
+                                <td width="5%">{{ $k+1 }}. </td>
+                                <td width="15%"><strong>Nama</strong></td>
+                                <td>: {{ $e['name'] }}</td>
+                                <td width="10%">
+                                    {{--  <a href="#" class="btn btn-flat btn-xs btn-default"><i class="fa fa-remove"></i></a>  --}}
+                                </td>
+                            </tr>
+
+                            {{--  <tr>
+                                <td></td>
+                                <td><strong>Pangkat</strong></td>
+                                <td>: [Pangkat], [Golongan] </td>
+                                <td></td>
+                            </tr>  --}}
+
+                            <tr>
+                                <td></td>
+                                <td><strong>NIP / NRP</strong></td>
+                                <td>: {{ $e['nip'] }} | {{ $e['nrp'] }} </td>
+                                <td></td>
+                            </tr>
+
+                            <tr>
+                                <td></td>
+                                <td><strong>Jabatan</strong></td>
+                                <td>: {{ $e['jobtitle'] }}, {{ $e['institute'] }} </td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </li>
+            @endforeach
+        @endif
+    </ul>
+</div>
