0709779c5b68e45bbf59fe3e446d4f76701d290b
Willy Nugraha <willymiiee@gmail.com>
2018-03-25 12:05:14 +0700

    update qna views

--- resources/views/surat/ba_was2_qna.blade.php
+++ resources/views/surat/ba_was2_qna.blade.php
@@ -7,132 +7,161 @@
 @section('subjudul', 'Kejaksaan Agung Republik Indonesia')
 
 @section('stylesheets')
-<!-- JQueryUI Tabs -->
-<link rel="stylesheet" href="{{ asset('/vendor/core/jquery-ui/themes/humanity/jquery-ui.css')}}">
-<link rel="stylesheet" href="{{ asset('/vendor/core/jquery-ui/themes/humanity/theme.css')}}">
-<!-- UI Tool Tip -->
-<link rel="stylesheet" href="{{ asset('vendor/core/jquery-ui/themes/base/jquery-ui.css')}}">
-<link rel="stylesheet" href="{{ asset('vendor/core/templates/alpha/assets/css/tooltip.css')}}">
-  <!-- iCheck for checkboxes and radio inputs -->
-        <link rel="stylesheet" href="{{ asset('/vendor/core/admin-lte/plugins/iCheck/all.css')}}">
-        <!-- Select2 -->
-    <link rel="stylesheet" href="{{ asset('/vendor/core/select2/dist/css/select2.min.css')}}">
+    <!-- JQueryUI Tabs -->
+    <link rel="stylesheet" href="{{ asset('vendor/core/jquery-ui/themes/humanity/jquery-ui.css')}}">
+    <link rel="stylesheet" href="{{ asset('vendor/core/jquery-ui/themes/humanity/theme.css')}}">
+    <!-- UI Tool Tip -->
+    <link rel="stylesheet" href="{{ asset('vendor/core/jquery-ui/themes/base/jquery-ui.css')}}">
+    <link rel="stylesheet" href="{{ asset('vendor/lapdu/templates/alpha/assets/css/tooltip.css')}}">
+    <!-- iCheck for checkboxes and radio inputs -->
+    <link rel="stylesheet" href="{{ asset('vendor/core/admin-lte/plugins/iCheck/all.css')}}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('vendor/core/select2/dist/css/select2.min.css')}}">
 @endsection
 
 @section('content')
+    <!-- Default box -->
+    <div class="box">
+        <div class="box-header with-border">
+            <h1 class="box-title">KARTU WAWANCARA</h1>
+        </div>
 
-<!-- Default box -->
-<!-- Default box -->
-<div class="box">
-  <div class="box-header with-border">
+        {{-- <span class="description-text">(KLARIFIKASI)</span> --}}
 
-<h1 class="box-title">KARTU WAWANCARA</h1>
-</div>
+        <div class="box-body">
+            @include('lapdu::surat.partials._ba_was2_qna_create')
+        </div>
 
+        <div class="box-footer">
+            <!-- this row will not appear when printing -->
+            <div class="row no-print">
+                <div class="col-xs-12">
+                    <a href="javascript:void(0)" id="save-btn" class="btn btn-info">
+                        <i class="fa fa-save"></i> Simpan
+                    </a>
 
-   {{-- <span class="description-text">(KLARIFIKASI)</span> --}}
-  <div class="box-body">
-    @include('lapdu::surat.partials._ba_was2_qna_create')
+                    {{--  <a href='klarifikasi_proses' class="btn btn-success pull-right" data-toggle="tooltip" data-placement="top" title="Isi Kesimpulan atas wawancara subyek hukum terkait" style="margin-right: 5px;">
+                        <i class="fa fa-check-square-o"></i> Lanjut
+                    </a>  --}}
 
-  </div>
-
-   <div class="box-footer">
-    <!-- this row will not appear when printing -->
-    <div class="row no-print">
-      <div class="col-xs-12">
-        <a href='klarifikasi_proses' class="btn btn-success pull-right" data-toggle="tooltip" data-placement="top" title="Isi Kesimpulan atas wawancara subyek hukum terkait" style="margin-right: 5px;"><i class="fa fa-check-square-o"></i> Lanjut
-        </a>
-        <a href='klarifikasi_proses' class="btn btn-danger pull-right" style="margin-right: 5px;">
-          <i class="fa fa-exclamation-triangle"></i> Batal
-        </a>
-      </div>
+                    <a href="{{ route('lapdu.operator.klarifikasi.action', ['id' => $data->_id]) }}" class="btn btn-default pull-right" style="margin-right: 5px;">
+                        <i class="fa fa-mail-reply"></i> Proses Klarifikasi
+                    </a>
+                </div>
+            </div>
+            <!-- /.content -->
+        </div>
+        <!-- /.box-footer-->
     </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.box-footer-->
-
-</div> <!-- /.box -->
-
+    <!-- /.box -->
 @endsection
 
 @section('scripts')
-<!-- Tool Tip -->
-<script src="{{ asset('vendor/core/jquery-3.2.1.min')}}"></script>
-<script src="{{ asset('vendor/core/jquery-ui/jquery-ui.js')}}"></script>
-  <!-- iCheck 1.0.1 -->
-    <script src="{{ asset('/vendor/core/admin-lte/plugins/iCheck/icheck.min.js')}}"></script>
+    <!-- Tool Tip -->
+    <script src="{{ asset('vendor/core/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{ asset('vendor/core/jquery-ui/jquery-ui.js')}}"></script>
+    <!-- iCheck 1.0.1 -->
+    <script src="{{ asset('vendor/core/admin-lte/plugins/iCheck/icheck.min.js')}}"></script>
     <!-- FastClick -->
-    <script src="{{ asset('/vendor/core/fastclick/lib/fastclick.js')}}"></script>
+    <script src="{{ asset('vendor/core/fastclick/lib/fastclick.js')}}"></script>
     <!-- bootstrap datepicker -->
-    <script src="{{ asset('/vendor/core/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{ asset('vendor/core/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
 
-      <script src="{{ asset('/vendor/core/ckeditor/ckeditor.js')}}"></script>
-<!-- Select2 -->
-<script src="{{ asset('/vendor/core/select2/dist/js/select2.full.min.js')}}"></script>
+    <script src="{{ asset('vendor/core/ckeditor/ckeditor.js')}}"></script>
+    <!-- Select2 -->
+    <script src="{{ asset('vendor/core/select2/dist/js/select2.full.min.js')}}"></script>
 
-      <script>
+    <script>
         $(function () {
-          //-- JQueryUI Tabs
-   $( "#tabs" ).tabs({
-      collapsible: true
-    })
-          // Tooltip
-          $( document ).tooltip({
-            position: {
-              my: "center bottom-20",
-              at: "center top",
-              using: function( position, feedback ) {
-                $( this ).css( position );
-                $( "<div>" )
-                  .addClass( "arrow" )
-                  .addClass( feedback.vertical )
-                  .addClass( feedback.horizontal )
-                  .appendTo( this );
-              }
-            }
-          });
+            //-- JQueryUI Tabs
+            $( "#tabs" ).tabs({
+                collapsible: true
+            })
+            // Tooltip
+            $( document ).tooltip({
+                position: {
+                my: "center bottom-20",
+                at: "center top",
+                using: function( position, feedback ) {
+                    $( this ).css( position );
+                    $( "<div>" )
+                    .addClass( "arrow" )
+                    .addClass( feedback.vertical )
+                    .addClass( feedback.horizontal )
+                    .appendTo( this );
+                }
+                }
+            })
 
-          //iCheck for checkbox and radio inputs
-          $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-            checkboxClass: 'icheckbox_minimal-blue',
-            radioClass   : 'iradio_minimal-blue'
-          })
-          //Red color scheme for iCheck
-          $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-            checkboxClass: 'icheckbox_minimal-red',
-            radioClass   : 'iradio_minimal-red'
-          })
-          //Flat red color scheme for iCheck
-          $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-            checkboxClass: 'icheckbox_flat-green',
-            radioClass   : 'iradio_flat-green'
-          })
-          //Date picker
-        $('#datepicker').datepicker({
-          autoclose: true
-        })
-          //Date picker
-        $('#datepicker2').datepicker({
-          autoclose: true
-        })
-        //Date picker
-        $('#notadinas').datepicker({
-          autoclose: true
-        })
-        //Date picker
-        $('#klarifikasi').datepicker({
-          autoclose: true
-        })
-        //Initialize Select2 Elements
-        $('.select2').select2()
+            //iCheck for checkbox and radio inputs
+            $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+                checkboxClass: 'icheckbox_minimal-blue',
+                radioClass   : 'iradio_minimal-blue'
+            })
+            //Red color scheme for iCheck
+            $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+                checkboxClass: 'icheckbox_minimal-red',
+                radioClass   : 'iradio_minimal-red'
+            })
+            //Flat red color scheme for iCheck
+            $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+                checkboxClass: 'icheckbox_flat-green',
+                radioClass   : 'iradio_flat-green'
+            })
+            //Date picker
+            $('#interview_date').datepicker({
+                autoclose: true
+            })
+            //Date picker
+            $('#klarifikasi').datepicker({
+                autoclose: true
+            })
+            //Initialize Select2 Elements
+            $('.select2').select2()
 
-         // Replace the <textarea id="editor1"> with a CKEditor
-        // instance, using default configuration.
-        CKEDITOR.replace('editor1')
-        //bootstrap WYSIHTML5 - text editor
-        $('.textarea').wysihtml5()
-        })
-        CKEDITOR.replace('kesimpulan')
+            // Replace the <textarea id="editor1"> with a CKEditor
+            // instance, using default configuration.
+            CKEDITOR.replace('answer')
+            CKEDITOR.replace('kesimpulan')
 
-      </script>
+            let counter = 1
+
+            $('#save-btn').click(function() {
+                let data = {
+                    '_method': 'PUT',
+                    'interview_summary': CKEDITOR.instances.kesimpulan.getData()
+                }
+
+                $.post("{{ route('api.lapdu.report.update', ['id' => $data->_id]) }}", data)
+                    .done(function(res) {
+                        alert('Data berhasil disimpan!')
+                    })
+            })
+
+            $('#add-qna').click(function() {
+                let data = {
+                    'id': "{{ $data->_id }}",
+                    'witness': $('#witness').val(),
+                    'interview_date': $('#interview_date').val(),
+                    'question': $('#question').val(),
+                    'answer': CKEDITOR.instances.answer.getData(),
+                    'interviewer': "{{ Auth::user()->name }}"
+                }
+
+                $.post("{{ route('api.lapdu.qna.store') }}", data)
+                    .done(function(res) {
+                        $('<tr/>').append(
+                            $('<td/>').html(counter),
+                            $('<td/>').html(res.question),
+                            $('<td/>').html(res.answer),
+                            $('<td/>').html(res.witness)
+                        ).appendTo($('#list-qna'))
+
+                        $('#question').val('')
+                        CKEDITOR.instances.answer.setData('')
+                        counter++
+                    })
+            })
+        })
+    </script>
 @endsection
--- resources/views/surat/partials/_ba_was2_qna_create.blade.php
+++ resources/views/surat/partials/_ba_was2_qna_create.blade.php
@@ -1,71 +1,69 @@
   <div class="col-md-12">
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-          <!-- Sidebar user panel (optional) -->
-               <div class="user-panel">
-                 <div class="pull-left image">
-                   <img src="{{ asset('vendor/core/admin-lte/dist/img/user.png')}}" class="img-circle" alt="User Image">
-                 </div>
-                 <div class="pull-left info">
-                   <p>{{ Auth::user()->name }}</p>
-                   <!-- Status -->
-                   <p>Pewawancara</p>
-                 </div>
-               </div>
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset('vendor/core/admin-lte/dist/img/user.png')}}" class="img-circle" alt="User Image">
+            </div>
+
+            <div class="pull-left info">
+                <p>{{ Auth::user()->name }}</p>
+                <!-- Status -->
+                <p>Pewawancara</p>
+            </div>
         </div>
-        <div class="col-md-6">
-          <select class="form-control select2" style="width: 100%;">
-            <option selected="selected">Pilih Saksi Pelapor / Terlapor?</option>
+    </div>
+
+    <div class="col-md-6">
+        <select class="form-control select2" style="width: 100%;" id="witness">
+            <option selected="selected">Pilih Saksi Pelapor / Terlapor</option>
             <optgroup label="Saksi Pelapor">
-                <option>Nama Saksi 1</option>
-                <option>Nama Saksi 2</option>
-                <option>Nama Saksi 3</option>
-              </optgroup>
-              <optgroup label="Saksi Terlapor">
-                <option>Terlapor</option>
-                <option>Nama Saksi 1</option>
-                <option>Nama Saksi 2</option>
-              </optgroup>
-          </select>
-          <div class="form-group">
-          <br>
+                @foreach ($data->reporters as $r)
+                <option value="{{ $r['name'] }}">{{ $r['name'] }}</option>
+                @endforeach
+            </optgroup>
+            <optgroup label="Saksi Terlapor">
+                @foreach ($data->reporteds as $r)
+                <option value="{{ $r['name'] }}">{{ $r['name'] }}</option>
+                @endforeach
+            </optgroup>
+        </select>
+
+        <div class="form-group">
+            <br>
             <div class="input-group">
-             <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-              <input type="text" class="form-control" placeholder="Tgl Wawancara" id="datepicker">
+                <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                <input type="text" class="form-control" placeholder="Tgl Wawancara" id="interview_date">
             </div>
             <!-- /.input group -->
-          </div><!-- /date -->
-        </div>
-      </div>
+        </div><!-- /date -->
+    </div>
+</div>
 
 
-        <div class="col-md-12">
-              <!-- /.box-header -->
-              <div class="box-body pad">
-                <div class="form-group">
-                  <label>Pertanyaan: </label>
-                  <input type="text" class="form-control" placeholder="Kolom pertanyaan ...">
-                </div>
-                <label for="">Jawaban: </label>
-                <form>
-                      <textarea id="editor1" name="editor1" rows="10" cols="80">
-
-                      </textarea>
-
-                </form>
-              </div>
-              <div class="box-footer">
-                  <button type="submit" class="btn btn-primary pull-right">Kirim</button>
-                </div>
-
-                @include('lapdu::surat.partials._ba_was2_detail')
-
+<div class="col-md-12">
+    <!-- /.box-header -->
+    <div class="box-body pad">
+        <div class="form-group">
+            <label>Pertanyaan: </label>
+            <input type="text" class="form-control" placeholder="Kolom pertanyaan ..." name="question" id="question">
         </div>
 
-        <div class="col-md-12">
-       <label for="">Kesimpulan : </label>
-       <form>
-         <textarea id="kesimpulan" name="kesimpulan" rows="10" cols="80" title="Please provide your firstname.">
-         </textarea>
-       </form>
-       </div>
+        <label for="">Jawaban: </label>
+        <textarea id="answer" rows="10" cols="80">
+        </textarea>
+    </div>
+
+    <div class="box-footer">
+        <button type="submit" id="add-qna" class="btn btn-primary pull-right">Kirim</button>
+    </div>
+
+    @include('lapdu::surat.partials._ba_was2_detail')
+</div>
+
+<div class="col-md-12">
+    <label for="">Kesimpulan : </label>
+    <textarea id="kesimpulan" rows="10" cols="80" placeholder="Silahkan masukkan kesimpulan">
+    </textarea>
+</div>
