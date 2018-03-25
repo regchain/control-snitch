7e3c258727c24dbcc5a6219ad7ab11fc5e5fb051
Willy Nugraha <willymiiee@gmail.com>
2018-03-25 10:12:07 +0700

    update footer

--- resources/views/operator/footer.blade.php
+++ resources/views/operator/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0.0
+      <b>Version</b> 2.0.0
     </div>
-    <small>Copyright © 2018-2023 <a href="http://soerojo.com" target="_blank">Tonny Soerojo &amp; Rekan</a>. All rights reserved.</small> 
+    <small>Copyright © 2018-2023 <a href="http://soerojo.com" target="_blank">Tonny Soerojo &amp; Rekan</a>. All rights reserved.</small>
   </footer>
